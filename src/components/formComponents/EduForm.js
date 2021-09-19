aa84867b1b84b7d341ce9567ea1f74897fac416d
Jared M <mezquitaj@gmail.com>
2021-09-19 01:46:01 -0400

    MVP is up and running - finished initial Edu and WorkXp Components

--- src/components/formComponents/EduForm.js
+++ src/components/formComponents/EduForm.js
@@ -1,26 +1,64 @@
 import React from 'react'
 
-function EduForm() {
-    return (
-        <form id="edu-form">
-            <input 
-                type="text" 
-                name="school" 
-                placeholder="School"
-            /><br/>
-            <input 
-                type="text" 
-                name="major" 
-                placeholder="Major"
-            /><br/>
-            <input 
-                type="tel" 
-                name="year" 
-                placeholder="Year"
-            /><br/>
-            <button>Save</button>
-        </form>
-    )
+class EduForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            school: "",
+            major: "",
+            year: "",
+            isEdit: true
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})       
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({isEdit: !this.state.isEdit})
+    }
+
+    render() {
+        if(this.state.isEdit) {
+            return (
+                <form id="edu-form" onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text" 
+                        name="school" 
+                        placeholder="School"
+                        value={this.state.school}
+                        onChange={this.handleChange}
+                    /><br/>
+                    <input 
+                        type="text" 
+                        name="major" 
+                        placeholder="Major"
+                        value={this.state.major}
+                        onChange={this.handleChange}
+                    /><br/>
+                    <input 
+                        type="tel" 
+                        name="year" 
+                        placeholder="Year"
+                        value={this.state.year}
+                        onChange={this.handleChange}
+                    /><br/>
+                    <button>Save</button>
+                </form>
+            )
+        } else {
+            return (
+                <div id="edu-input">
+                    <p><span>School:</span>  {this.state.school}</p>
+                    <p><span>Major:</span>  {this.state.major}</p>
+                    <p><span>Year:</span>  {this.state.year}</p>
+                    <button className="edit-btn" onClick={this.handleSubmit}>edit</button>
+                </div>
+            )
+        }         
+    }
 }
 
 export default EduForm
--- src/components/formComponents/WorkExpForm.js
+++ src/components/formComponents/WorkExpForm.js
@@ -1,36 +1,83 @@
 import React from 'react'
 
-function WorkExpForm() {
-    return (
-        <form id="experience-form">
-            <input 
-                type="text" 
-                name="position" 
-                placeholder="Position"
-            /><br/>
-            <input 
-                type="text" 
-                name="company" 
-                placeholder="Company"
-            /><br/>
-            <input 
-                type="text" 
-                name="city" 
-                placeholder="City"
-            /><br/>
-            <input 
-                type="text" 
-                name="from" 
-                placeholder="From"
-            /><br/>
-            <input 
-                type="text" 
-                name="city" 
-                placeholder="To"
-            /><br/>
-            <button>Save</button>
-        </form>
-    )
+class WorkExpForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            position: "",
+            company: "",
+            city: "",
+            from: "",
+            to: "",
+            isEdit: true
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})       
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({isEdit: !this.state.isEdit})
+    }
+
+    render() {
+        if(this.state.isEdit){
+            return (
+                <form id="experience-form" onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text" 
+                        name="position" 
+                        placeholder="Position"
+                        value={this.state.position}
+                        onChange={this.handleChange}
+                    /><br/>
+                    <input 
+                        type="text" 
+                        name="company" 
+                        placeholder="Company"
+                        value={this.state.company}
+                        onChange={this.handleChange}
+                    /><br/>
+                    <input 
+                        type="text" 
+                        name="city" 
+                        placeholder="City"
+                        value={this.state.city}
+                        onChange={this.handleChange}
+                    /><br/>
+                    <input 
+                        type="text" 
+                        name="from" 
+                        placeholder="From"
+                        value={this.state.from}
+                        onChange={this.handleChange}
+                    /><br/>
+                    <input 
+                        type="text" 
+                        name="to" 
+                        placeholder="To"
+                        value={this.state.to}
+                        onChange={this.handleChange}
+                    /><br/>
+                    <button>Save</button>
+                </form>
+            )
+        } else {
+            return (
+                <div id="workExp-input">
+                <p><span>Position:</span>  {this.state.position}</p>
+                <p><span>Company:</span>  {this.state.company}</p>
+                <p><span>City:</span>  {this.state.city}</p>
+                <p><span>From:</span>  {this.state.from}</p>
+                <p><span>To:</span>  {this.state.to}</p>
+                <button className="edit-btn" onClick={this.handleSubmit}>edit</button>
+                </div>
+            )
+        }     
+    }
+
 }
 
 export default WorkExpForm
